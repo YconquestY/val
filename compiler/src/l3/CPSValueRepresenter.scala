47e7310ea9c03d9123c7fc7648ceeda0e53c4e06
Xu Boran <boran.xu@epfl.ch>
2024-03-19 19:20:03 +0100

    partial implementation of value rep

--- compiler/src/l3/CPSValueRepresenter.scala
+++ compiler/src/l3/CPSValueRepresenter.scala
@@ -1,11 +1,38 @@
 package l3
 
-import l3.{ HighCPSTreeModule as H}
+import l3.{ HighCPSTreeModule as H }
 import l3.{ LowCPSTreeModule  as L }
+import l3.{ L3Primitive as L3 }
+import l3.{ L3ValuePrimitive as CPS }
+import l3.{ L3TestPrimitive as CPST }
+import CL3Literal._
 
 object CPSValueRepresenter extends (H.Tree => L.Tree) {
-  def apply(tree: H.Tree): L.Tree = tree match {
-    // better enclose pattern matching with a new function
+  def apply(tree: H.Tree): L.Tree =
+    h2lVal(tree)
+
+  private def h2lVal(tree: H.Tree): L.Tree = tree match {
+    case H.LetC(cnts: Seq[H.Cnt], body: H.Body) =>
+      L.LetC(cnts.map {
+          case H.Cnt(name, args, e) =>
+            L.Cnt(name, args, h2lVal(e))
+          //case _ =>
+          //  throw new Exception("Arugment is not continuation!")
+      }, h2lVal(body))
+    case H.AppC(cnt: H.Name, args: Seq[H.Atom]) =>
+      L.AppC(cnt, args.map(rewrite))
+    
+    // Functions translation ignored for now
+    case H.LetF(funs: Seq[H.Fun], body: H.Body) =>
+      L.LetF(funs.map {
+          case H.Fun(name, retC, args, e) =>
+            L.Fun(name, retC, args, h2lVal(e))
+          //case _ =>
+          //  throw new Exception("Arugment is not function!")
+      }, h2lVal(body))
+    case H.AppF(fun: H.Name, retC: H.Name, args: Seq[H.Atom]) =>
+      L.AppF(rewrite(fun), retC, args.map(rewrite))
+    
     case H.LetP(n, L3.IntAdd, Seq(x, y), b) =>
       val x1 = Symbol.fresh("t")
       /*
@@ -17,11 +44,13 @@ object CPSValueRepresenter extends (H.Tree => L.Tree) {
       })
     // TODO
   }
-
+  
   private def rewrite(a: H.Atom): L.Atom = a match {
-    case n: H.Name => n
-    case H.IntLit(v) => (v.toInt << 1) | 1 // equivalent to `v * 2 + 1`
-    // TODO
+    case n: H.Name     => n // ???
+    case IntLit(v)     => (v.toInt << 1) | 1 // equivalent to `v * 2 + 1`
+    case CharLit(c)    => (c.toInt << 3) | 6 // 0b110
+    case BooleanLit(b) => if (b) 0x1A else 0xA
+    case UnitLit       => 0x2
   }
 
   private def tmpLetP(p: L.ValuePrimitive, args: Seq[L.Atom],
@@ -29,4 +58,6 @@ object CPSValueRepresenter extends (H.Tree => L.Tree) {
     val tmp = Symbol.fresh("t")
     L.LetP(tmp, p, args, body(tmp))
   }
+
+  //private def h2lFun
 }
